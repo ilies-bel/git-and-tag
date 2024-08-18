fb23ae06f4dd39d6981c3d85ce5a4d5c54a63aed
ilies-bel <beldjilali.ilies@gmail.com>
2024-08-18 23:11:34 +0200

    chore: add dynamic configuration

--- src/AxiosInstance.tsx
+++ src/AxiosInstance.tsx
@@ -3,6 +3,6 @@ import axios from "axios";
 export const axiosInstance = axios.create({
     baseURL: 'https://api.github.com',
     headers: {
-        Authorization: `token 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`,  // Replace with your GitHub token
+        Authorization: `token ${Config.githubToken}`,
     },
 });
